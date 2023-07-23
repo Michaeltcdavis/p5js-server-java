69387d0fa65ecfab2e7aa9ad2b23ab2696a56e9b
Michael Davis <michaeltcdavis@gmail.com>
2023-07-22 21:11:35 -0400

    add html to response

--- src/main/java/P5HttpHandler.java
+++ src/main/java/P5HttpHandler.java
@@ -1,7 +1,10 @@
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 
@@ -12,10 +15,25 @@ public class P5HttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange ex) throws IOException {
-        String response = "Hello World";
-        ex.sendResponseHeaders(200, response.length());
+
+        InputStream is = ex.getRequestBody();
+        while (is.read() != -1) {
+            is.skip(0x1000);
+        }
+        is.close();
+
+        Headers h =ex.getResponseHeaders();
+        h.set("content-type", "text/html");
+
         OutputStream os = ex.getResponseBody();
-        os.write(response.getBytes());
+        FileInputStream fs = new FileInputStream("src/main/public/index.html");
+        ex.sendResponseHeaders(200, 0);
+        final byte[] buffer = new byte[0x1000];
+        int count;
+        while ((count = fs.read(buffer)) >= 0) {
+            os.write(buffer, 0, count);
+        }
+        fs.close();
         os.close();
     }
 
